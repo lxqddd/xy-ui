486c22d23476cb5d0847f854555aad046b5f0d91
lxq <xiangqian_liu@163.com>
2022-02-12 17:15:45 +0800

    feat: 做一些基础配置

---
+++ src/components/button/button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="button">
+    {{ props.type }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+export type buttonType = 'primary' | 'info' | 'success' | 'warning' | 'danger'
+export interface IButtonPropsType {
+  type: buttonType
+}
+
+const props = defineProps<IButtonPropsType>()
+console.log(props.type)
+</script>
---
+++ src/components/button/types.ts
@@ -0,0 +1,5 @@
+export type buttonType = 'primary' | 'info' | 'success' | 'warning' | 'danger'
+
+export interface IButtonPropsType {
+  type: buttonType
+}
