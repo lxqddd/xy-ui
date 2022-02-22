c42910109633020baceb6fd71c44af091ce856a2
lxq <xiangqian_liu@163.com>
2022-02-22 23:44:09 +0800

    feat: 头像组件接口定义

--- src/components/avatar/avatar.vue
+++ src/components/avatar/avatar.vue
@@ -2,6 +2,34 @@
   <div>这是头像组件</div>
 </template>
 
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+
+type sizeType = 'small' | 'medium' | 'large'
+
+interface IAvatarProps {
+  size?: sizeType | number
+  bgColor?: string
+  failBackSrc?: string
+  bordered?: boolean
+  objectFit?: 'file' | 'contain' | 'cover' | 'none' | 'scale-down'
+  round?: boolean
+  onError?: (e: Event) => void | undefined
+}
+
+const props = withDefaults(defineProps<IAvatarProps>(), {
+  size: 'medium',
+  bgColor: '#FFF',
+  failBackSrc: '',
+  bordered: true,
+  objectFit: 'file',
+  round: true,
+  onError: undefined
+})
+
+console.log(props.size)
+</script>
+
 <script lang="ts">
 export default { name: 'XyAvatar' }
 </script>
